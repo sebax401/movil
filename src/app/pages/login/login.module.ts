2a44ed68e2c2d1405de8ece7f57c47d4a8142d4c
sebastian cabrera <128442554+sebax401@users.noreply.github.com>
2024-10-29 14:15:37 -0300

    Add files via upload

---
+++ src/app/pages/login/login.module.ts
@@ -0,0 +1,23 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { LoginPageRoutingModule } from './login-routing.module';
+
+import { LoginPage } from './login.page';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LoginPageRoutingModule,
+    SharedModule
+  ],
+  declarations: [LoginPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class LoginPageModule {}
